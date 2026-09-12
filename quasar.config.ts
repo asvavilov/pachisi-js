15358f106bf371924bc039609dc692a35aaeeaad
asvavilov <vavilov.rf@gmail.com>
2026-09-12 23:40:19 +0300

    8.6: удалён неиспользуемый @intlify/unplugin-vue-i18n (runtime createI18n оставлен)

--- quasar.config.ts
+++ quasar.config.ts
@@ -2,7 +2,6 @@
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file
 
 import { defineConfig } from '#q-app';
-import { fileURLToPath } from 'node:url';
 
 export default defineConfig((ctx) => {
   return {
@@ -76,23 +75,6 @@ export default defineConfig((ctx) => {
       // viteVuePluginOptions: {},
 
       vitePlugins: [
-        [
-          '@intlify/unplugin-vue-i18n/vite',
-          {
-            // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-            // compositionOnly: false,
-
-            // if you want to use named tokens in your Vue I18n messages, such as 'Hello {name}',
-            // you need to set `runtimeOnly: false`
-            // runtimeOnly: false,
-
-            ssr: ctx.modeName === 'ssr',
-
-            // you need to set i18n resource including paths !
-            include: [fileURLToPath(new URL('./src/i18n', import.meta.url))],
-          },
-        ],
-
         [
           'vite-plugin-checker',
           {
